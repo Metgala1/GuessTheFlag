ee5c537130cc0364e4252ce0ffb18484303170c9
ÄTAGWE ROGER <atagweroger2@gmail.com>
2026-06-28 08:49:00 +0100

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Atagwe Roger on 28/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
