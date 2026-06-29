7ca55745af9f2573161b20cf20366d027f74f406
ÄTAGWE ROGER <atagweroger2@gmail.com>
2026-06-29 08:21:01 +0100

    Ädded Some text vies and buttons containing Images of three randomly choosen country flags

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAlert = false
+   var countries = ["Estonia", "France" , "Germany" , "Ireland" , "Italy" , "Nigeria" , "Poland" , "Spain" , "UK" , "Ukrain" , "US"]
+    var correctAnswer = Int.random(in: 0...2)
     var body: some View {
-        
+        ZStack{
+            Color.blue
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+                VStack{
+                    Text("Tap the flag of")
+                        .foregroundStyle(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(.white)
+                }
+                ForEach(0..<3) { number in
+                    Button {
+                        //flag was tapped
+                    }label: {
+                        Image(countries[number])
+                    }
+                }
+            }
+        }
+     
         }
     
     
